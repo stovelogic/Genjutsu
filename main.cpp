ebec18830db01808fc24e3b8af804de740e66eb4
AJ Minich <ajminich@gmail.com>
2011-09-25 21:51:20 -0700

    Messing around with printf

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
-#include <iostream>
+#include <stdio.h>
 
-using namespace std;
-
+/* 
+ * A basic test of using g++ with git and a vim editor.
+ * This is literally the first time I've ever used vim
+ * for editing code, so that's why this is pretty rough...
+ */
 int main()
 {
-	cout << "Hello world!" << endl;
+	printf("Hello, the number is %d.\n", 2);	
+
+
 	return 0;
 }
 
